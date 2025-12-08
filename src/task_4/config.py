cab707f9f655cf38b3432b2f249f15f9586dc447
Alex <alex@Alexs-MacBook-Air.local>
2025-12-09 01:42:29 +0300

    Fixed bug in config.py.

--- src/task_4/config.py
+++ src/task_4/config.py
@@ -3,7 +3,7 @@ import torch
 # Dataset settings
 DATASET_NAME = "ModelNet10"
 NUM_POINTS = 1024
-DATA_H5_PATH = "data/modelnet10_1024.h5"
+DATA_PATH = "data/modelnet10_1024.h5"
 MODELNET_RAW_PATH = "ModelNet10"
 
 # Model settings
@@ -12,13 +12,13 @@ FEATURE_TRANSFORM = True
 
 # Training settings
 BATCH_SIZE = 32
-NUM_EPOCHS = 25
+NUM_EPOCHS = 50
 TRAIN_FRAC = 0.8
 LEARNING_RATE = 0.001
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Output settings
-OUTPUT_DIR = "outputs"
+OUTPUT_DIR = "data"
 MODEL_SAVE_PATH = f"{OUTPUT_DIR}/pointnet_{DATASET_NAME.lower()}.pth"
 
 # Visualization
